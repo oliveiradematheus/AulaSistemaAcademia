ed145db7fb528f6ba09e0d19cad832a0596e7880
oliveiradematheus <matheusoliveirasilva390@gmail.com>
2024-05-15 21:34:32 -0300

    Métodos de conexão

--- SistemaAcademia/src/dao/Conexao.java
+++ SistemaAcademia/src/dao/Conexao.java
@@ -3,6 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
 /**
  *
@@ -14,6 +18,27 @@ public class Conexao {
     private static final String URL = "jdbc:mysql://localhost:3306/academia?zeroDateTimeBehavior=CONVERT_TO_NULL"; 
     private static final String USER = "root"; 
     private static final String PASS = "Senha1234"; 
+    
+    public static Connection getConnection () throws ClassNotFoundException, SQLException {
+        Class.forName(DRIVER);
+        return DriverMananger.getConnection(URL, USER, PASS);
+    };
+    
+    public static void closeConnection (Connection c) throws SQLException {
+        if ( c != null && !c.isClosed()) {
+            c.close();
+            c.commit();
+        }
+        
+    }
+    
+    public static void closeConnection (Connection c, PreparedStatement statement) throws SQLException {
+        closeConnection(c);
+        if (statement != null) {
+            statement.close();
+        }
+    };
+    
    
     
 }
